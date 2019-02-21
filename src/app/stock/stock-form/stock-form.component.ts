f80f28f4e862f5e81f5714052d9cddb365115d5c
Dawn <15021677496@163.com>
2019-02-22 01:33:32 +0800

    7.0 一半

--- src/app/stock/stock-form/stock-form.component.ts
+++ src/app/stock/stock-form/stock-form.component.ts
@@ -9,20 +9,37 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class StockFormComponent implements OnInit {
 
+  formModel:FormGroup;
+
   stock: Stock;
   private router: any;
+  categories = ["it", "互联网", "金融"]
 
   constructor(private  routeInfo: ActivatedRoute, private stockService: StockService) { }
 
   ngOnInit() {
     let stockId = this.routeInfo.snapshot.params['id'];
     this.stock = this.stockService.getStock(stockId);
+
+    let fb = new FormBuilder();
+    this.formModel = fb.group(
+      {
+        name:[this.stock.name, [Validators.required, Validators.minLength(3)]],
+        price:[this.stock.price, Validators.required],
+        desc: [this.stock.desc],
+        categories: fb.array([
+          new FormControl(this.stock.categories.indexOf(categories[0]) != -1),
+          new FormControl(this.stock.categories.indexOf(categories[1]) != -1),
+          new FormControl(this.stock.categories.indexOf(categories[2]) != -1)
+        ])
+      }
+    )
   }
 
   cancel(){
     this.router.navigateByUrl('/stock');
   }
   save(){
-
+    this.formModel.value.rating = this.stock.rating;
   }
 }
