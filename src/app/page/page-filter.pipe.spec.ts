b095a3c674833b9fe586a5d921b98e6f67c99ac8
Dawnfyx <15021677496@163.com>
2019-03-21 16:59:28 +0800

    19/03/21

--- src/app/page/page-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PageFilterPipe } from './page-filter.pipe';
-
-describe('PageFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PageFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
